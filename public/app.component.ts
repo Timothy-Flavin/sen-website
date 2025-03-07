2a7a05fa7e05d5e4a478191cfb1a7415a6c99e1b
Timothy Flavin <BinaryOwlizard@gmail.com>
2025-03-07 17:35:38 -0600

    blah

--- public/app.component.ts
+++ public/app.component.ts
@@ -3,13 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../src/app/footer/footer.component";
 import { HomeComponent } from "../src/app/home/home.component";
 import { HeaderComponent } from "../src/app/header/header.component";
+import { MembersComponent } from "../src/app/members/members.component";
+import { NgIf } from '@angular/common';
+import { PublicationsComponent } from "../src/app/publications/publications.component";
+import { ProjectsComponent } from '../src/app/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HomeComponent, HeaderComponent, NgIf, MembersComponent, PublicationsComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sen-website';
+  state = "home"
+  handleStateUpdate(target: string) {
+    console.log("we got the target" + target)
+    this.state = target
+  }
+  isHome() {
+    console.log("yeet yeet")
+    return this.state === 'home'
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  current_page: string = "home"
+  pages: string[] = ["home", "publications", "projects", "students", "gallery"]
+  @Output() stateChange = new EventEmitter<any>();
+  // When menu buttons are clicked change the page
+  changeState(target: string) {
+    if (this.pages.includes(target)) {
+      this.current_page = target
+      this.stateChange.emit(target)
+      //console.log(this.current_page)
+    }
+    else {
+      throw new Error("Target Page is not in page list")
+    }
+  }
 }
