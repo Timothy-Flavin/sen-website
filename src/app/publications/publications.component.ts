4050c5bcb6eef7f424085eea28f638c87cac95f8
Timothy Flavin <BinaryOwlizard@gmail.com>
2025-03-13 15:28:51 -0500

    publications comes from the database now

--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -11,9 +11,24 @@ import { NgFor, NgIf } from '@angular/common';
 })
 export class PublicationsComponent {
   publications: any[] = [];
+  display_publications: any[] = [];
   active: boolean[] = [];
+  loading: boolean = true;
+  ascending: boolean = true;
+  active_project_type = 0;
+  projects: any[] = [
+    { id: 0, name: 'Any', description: 'All Papers' },
+    { id: 1, name: 'Unassigned', description: 'Publications which do not fall into common lab categories' },
+    { id: 2, name: 'RL', description: 'Reinforcement Learning and Multi-Agent Reinforcement learning' },
+    { id: 3, name: 'XAI', description: 'Explainable and Interpretable AI' },
+    { id: 3, name: 'Genetic Algorithms', description: 'Explainable and Interpretable AI' },
+  ]
+  students: any[] = [
+    { id: 1, name: 'Ethan Beaird', photo: '/students/default-student.jpg' },
+  ];
   // Get the publications from the API
   ngOnInit() {
+    this.loading = true;
     this.get_publications();
     for (let i = 0; i < this.publications.length; i++) {
       this.publications[i].i = i;
@@ -28,6 +43,8 @@ export class PublicationsComponent {
     this.webApiService.getPublications("http://localhost:3000").subscribe((response) => {
       console.log(response)
       this.publications = response.body;
+      this.loading = false;
+      this.update_display_publications();
     });
   }
   get_active(i: number) {
@@ -38,5 +55,28 @@ export class PublicationsComponent {
   }
   update_active(i: number) {
     this.active[i] = !this.active[i];
+  }
+  sort_date(order: string) {
+    if (order == 'asc') { this.ascending = true; }
+    else { this.ascending = false; }
+    console.log(this.ascending)
+    this.publications.sort((a, b) => {
+      return order === 'asc' ? new Date(a.date).getTime() - new Date(b.date).getTime() : new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+  }
+  update_active_project_type(e: any) {
+    console.log(e.target)
+    let i = parseInt(e.target.value);
+    this.active_project_type = i;
+    this.update_display_publications();
+  }
+  update_display_publications() {
+    console.log(this.active_project_type)
+    console.log("We are updating display")
+    this.display_publications = this.publications.filter((publication) => {
+      console.log(publication.projectID, this.active_project_type)
+      if (this.active_project_type == 0) return true;
+      return publication.projectID == this.active_project_type;
+    });
   }
 }
