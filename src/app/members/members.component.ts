0d7169d3b254717ee791da42af28a3622d1b9bd9
Timothy Flavin <BinaryOwlizard@gmail.com>
2025-03-13 18:06:33 -0500

    everything comes from the database now

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WebApiService } from '../Service/web-api.service';
 
 interface Student {
   name: string;
@@ -7,7 +8,7 @@ interface Student {
   website?: string;
   email: string;
   bio: string;
-  type: 'phd' | 'undergrad' | 'alumni';
+  type: 'Ph.D.' | 'Undergrad' | 'Alumni';
   isExpanded: boolean;
   nextSteps?: string;
   degree?: 'PhD' | 'BS' | 'MS';
@@ -21,217 +22,237 @@ interface Student {
   styleUrl: './members.component.css'
 })
 export class MembersComponent {
-  students: Student[] = [
-    {
-      name: 'Ethan Beaird',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      type: 'undergrad',
-      isExpanded: false
-    },
-    {
-      name: 'Jacob Brue',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      type: 'phd',
-      isExpanded: false
-    },
-    {
-      name: 'Mikey Ferguson',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      type: 'undergrad',
-      isExpanded: false
-    },
-    {
-      name: 'Timmy Flavin',
-      photo: '/students/timface.png',
-      website: 'https://timothy-flavin.github.io/',
-      email: 'timmy-flavin@utulsa.edu',
-      bio: 'Research area: Multi Agent Reinforcement Learning (MARL) for Ad-Hoc Teamplay',
-      type: 'phd',
-      isExpanded: false
-    },
-    {
-      name: 'Zachary Gaskins',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-      type: 'undergrad',
-      isExpanded: false
-    },
-    {
-      name: 'Zachary Gray',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
-      type: 'undergrad',
-      isExpanded: false
-    },
-    {
-      name: 'Marina Katoh',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
-      type: 'undergrad',
-      isExpanded: false
-    },
-    {
-      name: 'Bryan Lavender',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
-      type: 'phd',
-      isExpanded: false
-    },
-    {
-      name: 'Dale Peasley',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.',
-      type: 'phd',
-      isExpanded: false
-    },
-    {
-      name: 'Eric Peterson',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat.',
-      type: 'phd',
-      isExpanded: false
-    },
-    {
-      name: 'William Pittenger',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.',
-      type: 'undergrad',
-      isExpanded: false
-    },
-    {
-      name: 'Joe Shymanski',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'email@utulsa.edu',
-      bio: 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi.',
-      type: 'phd',
-      isExpanded: false
-    },
-    {
-      name: 'TestAlumni1',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni1@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'BS',
-      nextSteps: 'Assistant Professor at MIT'
-    },
-    {
-      name: 'TestAlumni2',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni2@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'PhD',
-      nextSteps: 'Assistant Professor at MIT'
-    },
-    {
-      name: 'TestAlumni3',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni3@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'MS',
-      nextSteps: 'Assistant Professor at UT'
-    },
-    {
-      name: 'TestAlumni4',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni4@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'BS',
-      nextSteps: 'Assistant Professor at UT'
-    },
-    {
-      name: 'TestAlumni5',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni5@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'PhD',
-      nextSteps: 'Assistant Professor at UT'
-    },
-    {
-      name: 'TestAlumni6',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni6@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'MS',
-      nextSteps: 'Assistant Professor at UT'
-    },
-    {
-      name: 'TestAlumni7',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni7@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'BS',
-      nextSteps: 'Assistant Professor at UT'
-    },
-    {
-      name: 'TestAlumni8',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni8@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'PhD',
-      nextSteps: 'Assistant Professor at UT'
-    },
-    {
-      name: 'TestAlumni9',
-      photo: '/students/default-student.jpg',
-      website: 'https://website.com',
-      email: 'alumni9@example.com',
-      bio: '',
-      type: 'alumni',
-      isExpanded: false,
-      degree: 'MS',
-      nextSteps: 'Assistant Professor at UT'
-    }
-  ];
+  getting_students: boolean = true;
+  // students: Student[] = [
+  //   {
+  //     name: 'Ethan Beaird',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  //     type: 'undergrad',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Jacob Brue',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  //     type: 'phd',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Mikey Ferguson',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  //     type: 'undergrad',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Timmy Flavin',
+  //     photo: '/students/timface.png',
+  //     website: 'https://timothy-flavin.github.io/',
+  //     email: 'timmy-flavin@utulsa.edu',
+  //     bio: 'Research area: Multi Agent Reinforcement Learning (MARL) for Ad-Hoc Teamplay',
+  //     type: 'phd',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Zachary Gaskins',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
+  //     type: 'undergrad',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Zachary Gray',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
+  //     type: 'undergrad',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Marina Katoh',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+  //     type: 'undergrad',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Bryan Lavender',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
+  //     type: 'phd',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Dale Peasley',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.',
+  //     type: 'phd',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Eric Peterson',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat.',
+  //     type: 'phd',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'William Pittenger',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.',
+  //     type: 'undergrad',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'Joe Shymanski',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'email@utulsa.edu',
+  //     bio: 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi.',
+  //     type: 'phd',
+  //     isExpanded: false
+  //   },
+  //   {
+  //     name: 'TestAlumni1',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni1@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'BS',
+  //     nextSteps: 'Assistant Professor at MIT'
+  //   },
+  //   {
+  //     name: 'TestAlumni2',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni2@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'PhD',
+  //     nextSteps: 'Assistant Professor at MIT'
+  //   },
+  //   {
+  //     name: 'TestAlumni3',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni3@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'MS',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   },
+  //   {
+  //     name: 'TestAlumni4',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni4@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'BS',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   },
+  //   {
+  //     name: 'TestAlumni5',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni5@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'PhD',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   },
+  //   {
+  //     name: 'TestAlumni6',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni6@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'MS',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   },
+  //   {
+  //     name: 'TestAlumni7',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni7@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'BS',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   },
+  //   {
+  //     name: 'TestAlumni8',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni8@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'PhD',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   },
+  //   {
+  //     name: 'TestAlumni9',
+  //     photo: '/students/default-student.jpg',
+  //     website: 'https://website.com',
+  //     email: 'alumni9@example.com',
+  //     bio: '',
+  //     type: 'alumni',
+  //     isExpanded: false,
+  //     degree: 'MS',
+  //     nextSteps: 'Assistant Professor at UT'
+  //   }
+  // ];
+  students: Student[] = [];
+  constructor(private webApiService: WebApiService) { }
+
+  ngOnInit() {
+    this.getting_students = true;
+    this.get_members();
+    console.log("in ngafter init")
+    console.log(this.students)
+    console.log("done ngafter init")
+  }
 
   toggleBio(student: Student) {
     student.isExpanded = !student.isExpanded;
+  }
+
+  get_members() {
+    this.getting_students = true;
+    return this.webApiService.getPublications("http://localhost:3000/students").subscribe((response) => {
+      this.students = response.body;
+      console.log(this.students)
+      this.getting_students = false;
+    });
   }
 }
--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -12,55 +12,108 @@ import { NgFor, NgIf } from '@angular/common';
 export class PublicationsComponent {
   publications: any[] = [];
   display_publications: any[] = [];
-  active: boolean[] = [];
   loading: boolean = true;
+  getting_publications: boolean = true;
+  getting_members: boolean = true;
+  getting_projects: boolean = true;
   ascending: boolean = true;
   active_project_type = 0;
-  projects: any[] = [
-    { id: 0, name: 'Any', description: 'All Papers' },
-    { id: 1, name: 'Unassigned', description: 'Publications which do not fall into common lab categories' },
-    { id: 2, name: 'RL', description: 'Reinforcement Learning and Multi-Agent Reinforcement learning' },
-    { id: 3, name: 'XAI', description: 'Explainable and Interpretable AI' },
-    { id: 3, name: 'Genetic Algorithms', description: 'Explainable and Interpretable AI' },
-  ]
-  students: any[] = [
-    { id: 1, name: 'Ethan Beaird', photo: '/students/default-student.jpg' },
-  ];
+  projects: any[] = []
+  members: any[] = [];
+
+  constructor(private webApiService: WebApiService) { }
+
   // Get the publications from the API
   ngOnInit() {
     this.loading = true;
     this.get_publications();
+    this.get_members();
+    this.get_projects();
+    console.log("in ngafter init")
+    console.log(this.publications)
+    console.log(this.display_publications)
+    console.log("done ngafter init")
+  }
+
+
+  get_publications() {
+    this.getting_publications = true;
+    this.webApiService.getPublications("http://localhost:3000/publications").subscribe((response) => {
+      console.log(response)
+      this.publications = response.body;
+      this.getting_publications = false;
+      this.add_websites();
+    });
+  }
+
+  get_members() {
+    this.getting_members = true;
+    return this.webApiService.getPublications("http://localhost:3000/members").subscribe((response) => {
+      console.log(response)
+      this.members = response.body;
+      this.getting_members = false;
+      this.add_websites();
+    });
+  }
+
+  get_projects() {
+    this.getting_projects = true;
+    this.webApiService.getPublications("http://localhost:3000/projects").subscribe((response) => {
+      console.log(response)
+      this.projects = response.body;
+      this.getting_projects = false;
+      this.add_websites();
+    });
+  }
+
+  add_websites() {
+    if (this.getting_members || this.getting_publications || this.getting_projects) {
+      console.log("Not ready to add websites")
+      return;
+    }
+    console.log("Adding websites")
+
     for (let i = 0; i < this.publications.length; i++) {
       this.publications[i].i = i;
       console.log(this.publications[i])
+      this.publications[i].websites = [];
+      this.publications[i].active = false;
+      // Get the website for each author
+      // Check if the author is in the members list and get their website
+      for (let author_num = 0; author_num < this.publications[i].authors.length; author_num++) {
+        let author = this.publications[i].authors[author_num];
+        this.publications[i].websites.push(null);
+        for (let m = 0; m < this.members.length; m++) {
+          console.log(author)
+          if (String(author).toLowerCase() == (String(this.members[m].LastName).toLowerCase() + ', ' + String(this.members[m].FirstName).toLowerCase())) {
+            this.publications[i].websites[author_num] = this.members[m].Website;
+          }
+        }
+      }
     }
-    this.active = new Array(this.publications.length).fill(false);
+    this.update_display_publications();
+    console.log(this.publications)
+    console.log(this.display_publications)
+    console.log("after adding websites ^ ")
+    this.loading = false;
   }
 
-  constructor(private webApiService: WebApiService) { }
 
-  get_publications() {
-    this.webApiService.getPublications("http://localhost:3000").subscribe((response) => {
-      console.log(response)
-      this.publications = response.body;
-      this.loading = false;
-      this.update_display_publications();
-    });
-  }
+
   get_active(i: number) {
-    return this.active[i];
+    return this.display_publications[i].active;
   }
   get_inactive(i: number) {
-    return !this.active[i];
+    return !this.display_publications[i].active;
   }
   update_active(i: number) {
-    this.active[i] = !this.active[i];
+    this.display_publications[i].active = !this.display_publications[i].active;
   }
   sort_date(order: string) {
     if (order == 'asc') { this.ascending = true; }
     else { this.ascending = false; }
     console.log(this.ascending)
-    this.publications.sort((a, b) => {
+    this.display_publications.sort((a, b) => {
       return order === 'asc' ? new Date(a.date).getTime() - new Date(b.date).getTime() : new Date(b.date).getTime() - new Date(a.date).getTime();
     });
   }
@@ -78,5 +131,6 @@ export class PublicationsComponent {
       if (this.active_project_type == 0) return true;
       return publication.projectID == this.active_project_type;
     });
+    this.sort_date(this.ascending ? 'asc' : 'desc');
   }
 }
